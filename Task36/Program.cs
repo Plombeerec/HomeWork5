437653a146d50edef8498b0a3f4b70fd6a9c2b64
Plombeerec <107250294+Plombeerec@users.noreply.github.com>
2022-07-09 20:38:57 +0300

    Task 36 Done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] ArrayAgain(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int FindSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i%2 ==0) sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите размер массива");
+int Size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение");
+int Min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение");
+int Max = Convert.ToInt32(Console.ReadLine());
+
+int[] MyBestArray = ArrayAgain(Size, Min, Max);
+ShowArray(MyBestArray);
+Console.WriteLine(FindSum(MyBestArray));
